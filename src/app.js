3fc62086b1d08f6c39b5e680de504a359f937354
funmiayinde <funmiayinde11@gmail.com>
2021-11-03 04:13:32 +0100

    update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,6 @@ export default intiDatabase()
     // .then(() => initBot())
     .then(() => loadRoutes(app))
     .then(async (app) => {
-        // await initBot();
         const server = await http.createServer(app)
             .listen(config.get('app.port'));
         console.log(`\n
